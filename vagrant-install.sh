c9cf9463851d7fe2068ca92b540d8e1dc2b94c72
Maryan Bagnyuk <marbuga@gmail.com>
2019-06-28 10:14:12 +0300

    add vagrant usage

---
+++ vagrant-install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+/vagrant/vagrant/ubuntu16/scripts/nginx-install.sh
+# /vagrant/vagrant/ubuntu16/scripts/nodejs-v10-install.sh
+/vagrant/vagrant/ubuntu16/scripts/nodejs-v11-install.sh
+# /vagrant/vagrant/ubuntu16/scripts/nodejs-v6-install.sh
+# /vagrant/vagrant/ubuntu16/scripts/nodejs-v8-install.sh
