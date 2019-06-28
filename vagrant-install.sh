c453fe5b35465fcc1a73dc11dc816a8673314ec7
Maryan Bagnyuk <marbuga@gmail.com>
2019-06-28 11:51:01 +0300

    install nginx config

--- vagrant-install.sh
+++ vagrant-install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 /vagrant/vagrant/ubuntu16/scripts/nginx-install.sh
+/vagrant/vagrant/ubuntu16/scripts/nginx-config-install.sh
+
 # /vagrant/vagrant/ubuntu16/scripts/nodejs-v10-install.sh
 /vagrant/vagrant/ubuntu16/scripts/nodejs-v11-install.sh
 # /vagrant/vagrant/ubuntu16/scripts/nodejs-v6-install.sh
---
+++ vagrant/ubuntu16/scripts/nginx-config-install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+sudo cp ./fs/etc/nginx/sites-available/default /etc/nginx/sites-available/default
+sudo apt-get install -y nginx
